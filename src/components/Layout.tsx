2c4c85e15e09be0ac6707034671c35ae836c1ee9
David Kang <davidkang@Davids-MacBook-Pro.local>
2023-05-16 01:50:23 -0700

    add back button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,10 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 import React from 'react'
 import WifiIcon from '@mui/icons-material/Wifi';
 import Battery6BarIcon from '@mui/icons-material/Battery6Bar';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Link from 'next/link';
 
 export type LayoutProps = {
     children: React.ReactNode
@@ -14,12 +16,13 @@ function Layout({ children }: LayoutProps) {
     const router = useRouter();
     const isDetailPage = router.asPath.includes('detail');
     const title = isDetailPage ? decodeURIComponent(router.asPath.split('/')[2] as string) : 'Thirsty';
-    
+
     return (
         <>
             <Box
                 bgcolor='whitesmoke'
-                borderBottom='1px solid lightgray'>
+                borderBottom='1px solid lightgray'
+                position='relative'>
                 <Box
                     display='flex'
                     justifyContent='space-between'
@@ -33,10 +36,31 @@ function Layout({ children }: LayoutProps) {
                         {currentTime}
                     </Typography>
                     <Stack direction="row" spacing={1}>
-                        <WifiIcon fontSize='small' sx={{ marginRight: '-4px' }} />
-                        <Battery6BarIcon fontSize='small' sx={{ transform: 'rotate(90deg)' }} />
+                        <WifiIcon
+                            fontSize='small'
+                            sx={{ marginRight: '-4px' }} />
+                        <Battery6BarIcon
+                            fontSize='small'
+                            sx={{ transform: 'rotate(90deg)' }} />
                     </Stack>
                 </Box>
+                {isDetailPage && (
+                    <Link href="/">
+                        <Button
+                            startIcon={<ArrowBackIosIcon sx={{ marginRight: '-10px' }} />}
+                            sx={{
+                                bottom: 4,
+                                left: 14,
+                                position: 'absolute',
+                                '&:hover': {
+                                    backgroundColor: 'unset',
+                                },
+                            }}>
+                            <Typography textTransform='none'>Thirsty</Typography>
+                        </Button>
+                    </Link>
+
+                )}
                 <Typography
                     fontSize='18px'
                     fontWeight='bold'
